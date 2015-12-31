3169a6ef092835e5c1ebf734e4194b4527250a04
Swati Kamtar <kamtar.swati@gmail.com>
2015-12-30 18:53:41 -0800

    test: Fix 'Initial Entries' suite to throw jasmine error.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,137 +26,100 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         var feedItem;
-         function test(feed){
-            return feed;
-         }
-         it(' have defined and non empty URLs', function(){
+        it(' have defined and non empty URLs', function(){
             allFeeds.forEach(function(feed){
-                feedItem = test(feed);
-                expect(feedItem.url).toBeDefined();
-                expect(feedItem.url).not.toBe("");
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe("");
             });
+        });
 
-         });
-
-
-
-         /* Test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-
-            it('have defined and non empty names', function(){
-                allFeeds.forEach(function(feed){
-                    feedItem = test(feed);
-                });
-                expect(feedItem.name).toBeDefined();
-                expect(feedItem.name).not.toBe("");
+        it('have defined and non empty names', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe("");
             });
+        });
     });
 
-
     /*  A new test suite named "The menu" */
-
     describe('The menu', function() {
         var body = $('body'),
             menuIcon = $('.menu-icon-link'),
             menuPanel = $('.feed-list');
 
-         /* Test that ensures the menu element is
+        /* Test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
         it(' is hidden by default', function(){
             expect(body).toHaveClass('menu-hidden');
         });
 
-         /* Test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
+        /* Test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
         it(' changes visibility when the menu icon is clicked', function(){
             menuIcon.click();
             expect(body).not.toHaveClass('menu-hidden');
-
             menuIcon.click();
             expect(body).toHaveClass('menu-hidden');
         });
-
-        /* Test that ensures the menu hides when an element
-         * from the feed-list is chosen
-         */
-
-        it(' hides when clicked on a menu-item', function(){
-            var spyEvent = spyOnEvent(menuPanel.children('li'), 'click');
-            menuPanel.children('li').click();
-            expect(spyEvent).toHaveBeenTriggered();
-            expect(body).toHaveClass('menu-hidden');
-         });
-
     });
-
-
 
     /* A new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
-       /* A test that ensures when the loadFeed
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
-
-         beforeEach(function(done){
+        var $feed;
+        beforeEach(function(done){
             loadFeed(0, done);
-         });
-
-         var feed = $('.feed');
-         it(' has atleast one entry element', function(){
-            expect(feed[0].children[0].children[0]).toBeDefined();
-            expect(feed[0].children[0].children[0]).toHaveClass('entry');
-         });
+        });
+        it(' has atleast one entry element', function(){
+            $feed = $('.feed .entry');
+            expect($feed.length).toBeGreaterThan(0);
+        });
     });
 
-
-    /* A new test suite named "New Feed Selection"
-
+    /* A new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function(){
+        var feedOne, feedTwo;
+        beforeEach(function(done){
+            $('.feed').empty();
+            loadFeed(1, function(){
+                feedOne = $('.feed').find('h2').text();
+                done();
+            });
+        });
         /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        describe('New Feed Selection', function(){
-            var feed_one, feed_two;
-            beforeEach(function(done){
-                $('.feed').empty();
-                loadFeed(1, function(){
-                    feed_one = $('.feed').find('h2').text();
-                    done();
-                });
-
-            });
-            it(' actually changes the content', function(done){
-                $('.feed').empty();
-                loadFeed(0, function(){
-                    feed_two = $('.feed').find('h2').text();
-                    console.log(feed_one);
-                    console.log(feed_two);
-                    expect(feed_one).not.toEqual("");
-                    expect(feed_two).not.toEqual("");
-                    expect(feed_one).not.toEqual(feed_two);
-                    done();
-                });
-
+        it(' actually changes the content', function(done){
+            $('.feed').empty();
+            loadFeed(0, function(){
+                feedTwo = $('.feed').find('h2').text();
+                console.log(feedOne);
+                console.log(feedTwo);
+                expect(feedOne).not.toEqual("");
+                expect(feedTwo).not.toEqual("");
+                expect(feedOne).not.toEqual(feedTwo);
+                done();
             });
         });
+    });
 }());
